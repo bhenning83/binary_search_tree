08ae185a0a36952271b36ac3c52d1b7881266f0a
Brendon Henning <brendonhenning@gmail.com>
2020-08-08 22:45:27 -0500

    finished

--- knights_travels.rb
+++ knights_travels.rb
@@ -10,38 +10,54 @@
 # return the path of the nodes that found the value
 require 'pry'
 class Knight
-  attr_accessor :current, :target
+  attr_accessor :current, :target, :positions
 
   def initialize(current, target)
     @current = Node.new(current)
     @target = target
     @moves = [[-1, 2], [-1, -2], [-2, -1], [-2, 1], [1, 2], [1, -2], [2, 1], [2, -1]]
+    @target_reached = false
+    @positions = []
   end
 
   def move_knight(current = @current.coord, pos_move = [])
-    ary = []
     @moves.each do |move|
       current.each_index do |i|
          pos_move[i] = current[i] + move[i]
-      end
-      new_spot = Node.new(pos_move.dup, @current) if on_board?(pos_move)
-      ary << new_spot
-      match?(new_spot)
+        end
+      next if !on_board?(pos_move)
+      new_spot = Node.new(pos_move.dup, @current) 
+      @positions << new_spot
     end
   end
 
-  def match?(node, queue = [])
-    coord = node.coord
-    if target.eql?(coord)
-      queue << target
-      node = node.parent
-      until node.nil?
-        queue << node.coord 
-        node = node.parent
+  def check_for_match(ary = @positions)
+    ary.each do |node|
+      if target.eql?(node.coord)
+        print_route(node)
+        @target_reached = true
       end
-      p queue.reverse
-      exit!
     end
+  end
+  
+  def move_again(ary = @positions.dup)
+    @positions = []
+    ary.each do |node|
+      @current = node
+      move_knight
+    end
+  end
+
+  def print_route(node, ary = [])
+    until node.nil?
+      ary << node.coord 
+      node = node.parent
+    end
+    ary.reverse!
+    for i in 0..ary.length - 2
+      print "#{ary[i]} --> "
+    end
+    print "#{ary[-1]}\n"
   end
 
   def on_board?(coord = @coord)
@@ -51,6 +67,13 @@ class Knight
     true
   end
 
+  def find_route
+    until @target_reached == true do 
+      move_knight
+      check_for_match
+      move_again
+    end
+  end
 end
 
 class Node
@@ -62,7 +85,7 @@ class Node
   end
 end
 
-test = Knight.new([3, 4], [4, 2])
+test = Knight.new([1, 2], [8, 7])
 
-test.move_knight
+test.find_route
 
